6ea729af7b11db8a4f24874e53ac1504f29b8b25
WangRenjun <wangrenjun12@sina.com>
2017-05-25 17:54:36 +0800

    增加rabbitmq

---
+++ src/main/java/me/wrj/haifa/commons/csv/CSVTest.java
@@ -0,0 +1,25 @@
+package me.wrj.haifa.commons.csv;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
+import java.io.Reader;
+
+/**
+ * Created by wangrenjun on 2017/5/22.
+ */
+public class CSVTest {
+
+    public static void main(String[] args) throws  Exception {
+        System.getenv();
+        Reader in = new FileReader("data/file.csv");
+        Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+        for (CSVRecord record : records) {
+            String lastName = record.get("Last Name");
+            String firstName = record.get("First Name");
+            System.out.printf("lastName =  %s, firstName= %s",lastName,firstName);
+        }
+
+    }
+}
