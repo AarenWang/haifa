362496cc1989817fdb7557a9d7d8a6f96e3858e0
AarenWang <wangrenjun12@sina.com>
2024-09-08 16:38:32 +0800

    flink

--- haifa-flink/src/main/java/me/wrj/flink/fraud/FraudDetectionJob.java
+++ haifa-flink/src/main/java/me/wrj/flink/fraud/FraudDetectionJob.java
@@ -1,21 +1,26 @@
 package me.wrj.flink.fraud;
 
-import me.wrj.flink.fraud.source.TransactionSource;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import me.wrj.flink.fraud.sink.AlertSink;
+import me.wrj.flink.fraud.entity.Alert;
+import me.wrj.flink.fraud.entity.Transaction;
+import me.wrj.flink.fraud.source.TransactionSource;
 
-import javax.security.auth.login.AppConfigurationEntry;
+import java.util.HashMap;
+import java.util.Map;
 
-
+/**
+ * Skeleton code for the datastream walkthrough
+ */
 public class FraudDetectionJob {
     public static void main(String[] args) throws Exception {
-//        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        Configuration configuration = new Configuration();
-        configuration.setInteger("rest.port", 8081);
-
-
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(configuration);
+        //StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        Map<String, String> map  = new HashMap<>();
+        map.put("rest.flamegraph.enabled","true");
+        Configuration conf = Configuration.fromMap(map);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
 
         DataStream<Transaction> transactions = env
                 .addSource(new TransactionSource())
